f29524d1ac00beed167f30b22f10526bb12abe06
jrlvarghese <jrlvarghese@gmail.com>
2024-02-21 21:44:24 +0530

    Main menu logic added.

--- dryer_shutter_control.ino
+++ dryer_shutter_control.ino
@@ -14,8 +14,6 @@
 #define ROT_DAT 3
 #define ROT_CLK 4
 
-
-
 /* OUTPUT PINS */
 // LED DISPLAY
 #define DISP_A_DAT 9
@@ -27,6 +25,41 @@
 // Create a display object of type TM1637Display
 TM1637Display disp_current = TM1637Display(DISP_A_CLK, DISP_A_DAT);
 TM1637Display disp_set = TM1637Display(DISP_B_CLK, DISP_B_DAT);
+
+// Create a module for button
+OneButton button(ROT_SW, true, true); // input pullup activated and switch is active low
+
+/* VARIABLES ARE DECLARED HERE */
+bool rot_clk_present_state = true;
+volatile bool rot_clk_last_state = true;
+volatile bool menu_state = false;
+volatile bool selected = false;
+
+int menu_item = 0;
+int prev_menu_item = -1;
+
+int variable = 0;
+int prev_variable = -1;
+
+bool troubleshoot = false;
+
+unsigned long current_millis = 0;
+// time tracker for serial print
+unsigned long prev_serial_time = 0;
+unsigned long serial_interval = 1000;
+// time tracker for led display
+unsigned long prev_disp_time = 0;
+unsigned long disp_interval = 2000;
+// time tracker for sensors
+unsigned long prev_sense_time = 0;
+unsigned long sense_interval = 1000;
+// time tracker for menu
+unsigned long menu_max_time = 30000;
+unsigned long menu_timer = 0;
+// time tracker for submenu
+unsigned long submenu_max_time = 10000;
+unsigned long submenu_timer = 0;
+
 
 /* NUM/LETTER ARRAY FOR LED DISPLAY */
 // dash line on startup
@@ -93,7 +126,8 @@ const uint8_t he[] = {
 
 const uint8_t arr[2][1] = {{temp},{humid}};
 
-
+// for test purposes
+int i=0;
 
 void setup(){
     Serial.begin(9600);
@@ -106,7 +140,7 @@ void setup(){
     // setup input pins
     pinMode(ROT_CLK, INPUT);
     pinMode(ROT_DAT, INPUT);
-    pinMode(ROT_SW, INPUT);
+    // pinMode(ROT_SW, INPUT);
 
     // Set the LED display brightness to 5 (0=dimmest 7=brightest)
 	  disp_current.setBrightness(5);
@@ -118,71 +152,106 @@ void setup(){
     //clear display
 	  disp_current.clear();
     disp_set.clear();
-
-    // // attach long press function to button
-    // button.attachLongPressStop(long_press);
-    // // attach click function
-    // button.attachClick(on_click);
+    
+    // attach long press function to button
+    button.attachLongPressStop(long_press);
+    // attach click function
+    button.attachClick(on_click);
 }
 
 void loop(){
-    for(int i=0; i<100; i++){
-        if(i%2==0){
-            disp_set.clear();
-            disp_set.showNumberDec(i);
-            delay(1000);
-        }else{
-            disp_current.clear();
-            disp_current.showNumberDec(i);
-            delay(1000);
-        }
+  // get current millis
+  current_millis = millis();
+  // button tick is run always to detect button press
+  button.tick();
+
+  // update the display at predefined interval
+  if((prev_disp_time-current_millis)>disp_interval){
+    disp_current.clear();
+    disp_current.showNumberDec(i);
+    i++;
+    if(i>999)i=0;
+    prev_disp_time = current_millis;
+  }
+
+  //get the menu time, so that to track the time before entering menu
+  menu_timer = current_millis;
+  prev_menu_item = -1;
+  // MAIN MENU STARTS HERE
+  while(menu_state){
+    current_millis = millis();
+    // run button ticks to track button press
+    button.tick();
+    // update the menu selection while turning rotary encoder knob
+    menu_item = updateViaEncoder(menu_item, 0, 7);
+    // if there is change in menu selection update the display
+    if(prev_menu_item != menu_item){
+      show_menu_option(menu_item);
+      // reset the menu timer: if there is no change in menu items it remains the same
+      menu_timer = current_millis;
+      // update the previous menu item so that when it changes display get updated
+      prev_menu_item = menu_item;
     }
+    // if the menu remains inactive for a predefined time exit menu
+    if((current_millis - menu_timer)>menu_max_time){
+      menu_state = false;
+    }
+        
+  }//END OF WHILE LOOP FOR MAIN MENU
+  
     
 }
 
 
 /* FUNCTIONS */
 
-// // Function to be implemented after detecting long press
-// void long_press(){
-//     menu_state = !menu_state;
-// }
+// show menu selection 
+void show_menu_option(int menu_item){
+  disp_set.clear(); // clear the display
+  disp_set.setSegments(letter_p, 1, 0); // load letter p
+  disp_set.showNumberDec(menu_item+1, false, 1, 1); // load the menu number
+}
 
-// void on_click(){
-//     selected = !selected;
-//     // save to eeprom only if clicked inside a menu
-//     if(menu_state){
-//         write_EEPROM_after_check(menu_item, parameter_arr[menu_item]);
-//     }
-// }
+// Function to be implemented after detecting long press
+void long_press(){
+    menu_state = !menu_state;
+}
 
-// // check ticks 
-// void checkTicks(){
-//     button.tick();
-// }
+void on_click(){
+    selected = !selected;
+    // // save to eeprom only if clicked inside a menu
+    // if(menu_state){
+    //     write_EEPROM_after_check(menu_item, parameter_arr[menu_item]);
+    // }
+}
 
-// // function to modify given menu_item using rotary encoder
-// int updateViaEncoder(int menu_item, int min, int max){
-//     // Read present state of the rotary encoder pin A
-//     rot_clk_present_state = digitalRead(ROT_CLK);
-//     // Check rotation status and increment or decrement based on direction
-//     if(rot_clk_present_state != rot_clk_last_state && rot_clk_present_state == true){
-//         if(digitalRead(ROT_DAT) == rot_clk_present_state){
-//             menu_item++;
-//         }else{
-//             menu_item--;
-//         }
-//         //Serial.println(count);
-//     }
-//     // remember the last state
-//     rot_clk_last_state = rot_clk_present_state;
-//     //    delay(1);
-//     // check if menu_item exceeding the limits
-//     (menu_item>max)?menu_item=max:menu_item;
-//     (menu_item<min)?menu_item=min:menu_item;
+// check ticks 
+void checkTicks(){
+    button.tick();
+}
 
-//     return menu_item;
-// }
+// function to modify given menu_item using rotary encoder
+int updateViaEncoder(int menu_item, int min, int max){
+    // Read present state of the rotary encoder pin A
+    rot_clk_present_state = digitalRead(ROT_CLK);
+    // Check rotation status and increment or decrement based on direction
+    if(rot_clk_present_state != rot_clk_last_state && rot_clk_present_state == true){
+        if(digitalRead(ROT_DAT) == rot_clk_present_state){
+            menu_item++;
+        }else{
+            menu_item--;
+        }
+        //Serial.println(count);
+    }
+    // remember the last state
+    rot_clk_last_state = rot_clk_present_state;
+    //    delay(1);
+    // check if menu_item exceeding the limits
+    (menu_item>max)?menu_item=max:menu_item;
+    (menu_item<min)?menu_item=min:menu_item;
+
+    return menu_item;
+}
 
 // // shiftout function for shift register
 // void shiftOut(byte data){
